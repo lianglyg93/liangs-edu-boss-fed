8dfd9fdd00f4610322e78230ed9ae032b053034f
lianglyg93 <747726748@qq.com>
2021-08-17 00:47:06 +0800

    修改

---
+++ test-serve/app.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.use(
+  "/boss",
+  createProxyMiddleware({
+    target: "http://eduboss.lagou.com",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/front",
+  createProxyMiddleware({
+    target: "http://edufront.lagou.com",
+    changeOrigin: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("running...");
+});
